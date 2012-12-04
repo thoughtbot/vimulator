332b1ce2cbc65e0e101f6ba73606adffc702efe7
George Brocklehurst <george.brocklehurst@gmail.com>
2012-12-04 18:05:40 +0100

    Escape in insert mode is an Operation.

--- js/insert_mode.js
+++ js/insert_mode.js
@@ -1,4 +1,14 @@
 (function () {
+    var escapeCommand = new Vimulator.Command({
+        callback: function (vim) {
+            vim.setMode("normal");
+            vim.moveCursorRelative(0, -1);
+        },
+        description: function () {
+            return "<kbd>ESC</kbd> Return to normal mode";
+        }
+    });
+
     Vimulator.InsertMode = function (vim) {
         this.name = "insert";
         this.vim = vim;
@@ -9,10 +19,14 @@
     };
 
     Vimulator.InsertMode.prototype.keyPress = function (key) {
+        var op;
+
         // Escape
         if (key === '\u001B') {
-            this.vim.setMode("normal");
-            this.vim.moveCursorRelative(0, -1);
+            op = new Vimulator.Operation;
+            op.setCommand(escapeCommand);
+            op.execute(this.vim);
+            return op;
         } else {
             this.vim.registers["."] += key;
             this.vim.appendText(key);
